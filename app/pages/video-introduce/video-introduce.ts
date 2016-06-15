52b71d6e939ba48186956dac22aaa85ff02ecda7
mikedutuandu <mikedutuandu@gmail.com>
2016-06-15 13:50:41 +0700

    update code

--- app/pages/video-introduce/video-introduce.ts
+++ app/pages/video-introduce/video-introduce.ts
@@ -43,22 +43,43 @@ export class VideoIntroducePage {
     }
 
     startCamera(){
+
             this.showVideo = true;
-        var options = {
-            quality : 75,
-            destinationType : Camera.DestinationType.DATA_URL,
-            sourceType : Camera.PictureSourceType.CAMERA,
-            allowEdit : true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            saveToPhotoAlbum: false
+        // var options = {
+        //     quality : 75,
+        //     destinationType : Camera.DestinationType.FILE_URI,
+        //     sourceType : Camera.PictureSourceType.CAMERA,
+        //     targetWidth: 300,
+        //     targetHeight: 300,
+        //     saveToPhotoAlbum: false,
+        //     mediaType: Camera.MediaType.VIDEO
+        // };
+        // Camera.getPicture(options).then((imageData) => {
+        //     // imageData is either a base64 encoded string or a file URI
+        //     // If it's base64:
+        //     this.imgURI = "data:image/jpeg;base64," + imageData;
+        // }, (err) => {
+        // });
+
+        // capture callback
+        var captureSuccess = function(mediaFiles) {
+            var i, path, len;
+            for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+                path = mediaFiles[i].fullPath;
+                // do something interesting with the file
+            }
         };
-        Camera.getPicture(options).then((imageData) => {
-            // imageData is either a base64 encoded string or a file URI
-            // If it's base64:
-            this.imgURI = "data:image/jpeg;base64," + imageData;
-        }, (err) => {
+
+        // capture error callback
+        var captureError = function(error) {
+            navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+        };
+
+        var cordovaCapture = navigator.device.capture;
+        cordovaCapture.captureVideo(captureSuccess, captureError, {limit:1}).then(function(videoData) {
+            // Success! Video data is here
+        }, function(err) {
+            // An error occurred. Show a message to the user
         });
     }
 
