f1cdb981e0823e14c2f44e0ede9bbf7b85b7b79a
mikedutuandu <mikedutuandu@gmail.com>
2016-06-15 08:54:11 +0700

    update code

--- app/src/main/java/com/sunrise/interview/interviewspot/util/camera/CConstant.java
+++
@@ -1,7 +0,0 @@
-package com.sunrise.interview.interviewspot.util.camera;
-
-public class CConstant {
-	public static int puEncodingFormat = 0;
-	public static int puContainerFormat = 0;
-	public static int puResolutionChoice = 1;
-}
